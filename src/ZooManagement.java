6d55314930fbfb5c2d42ebb6ac66c15297bfef0d
Mahmoud-Belghaieb <mahmoud.belghaieb@gmail.com>
2023-12-02 19:41:11 +0100

    prosit2

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
 public class ZooManagement {
-    int nbrCages = 20;
-    String zooName = "myZoo";
+    //Prosit1:
+    /*int nbrCages = 20;
+    String zooName = "myZoo";*/
 
     public static void main(String[] args) {
-        //Instruction 1
-        ZooManagement zm = new ZooManagement();
+        //Prosit1:
+        /*ZooManagement zm = new ZooManagement();
         System.out.println(zm.zooName + " comporte " + zm.nbrCages + " cages.");
 
         ZooManagement zm1 = new ZooManagement();
@@ -22,5 +23,37 @@ public class ZooManagement {
 
 
         System.out.println(zm1.zooName + " Zoo comporte " + zm1.nbrCages + " cages.");
+    }*/
+
+        //Prosit 2:
+        Animal Lion = new Animal("Cats", "saba3", 15, true);
+
+        Zoo myZoo = new Zoo("jurassic park", "7affouz", 25);
+        myZoo.animals = new Animal[25];
+        Animal cat = new Animal("Tabby cat", "Puss in Boots", 2, true);
+
+        //INSTRUCTION8:
+        //myZoo.displayZoo();
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+        /*------------------------------------------------------------------------------*/
+
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+
+        System.out.println(myZoo.addAnimal(Lion));
+        System.out.println(myZoo.addAnimal(cat));
+
+        myZoo.displayAnimals();
+
+        System.out.println(myZoo.searchAnimal(Lion));
+        Animal Elgatto = new Animal("Mel chara3", "senior gatto", 51, true);
+        System.out.println(myZoo.searchAnimal(Elgatto));
+
+        System.out.println(myZoo.removeAnimal(cat));
+
+
+        myZoo.displayAnimals();
+
     }
 }
